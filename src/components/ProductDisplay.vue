71a0f17fc95d26dbdbd3cefa79962855c5390f1f
alfi <alfi@domainesia.com>
2023-05-06 19:19:31 +0700

    fix: (components/ProductDisplay) change title color from black to purple

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -8,7 +8,7 @@
         <div class="w-1/2 my-24 px-12 flex flex-col justify-between">
             <!-- layout 2a -->
             <div>
-                <h1 class="font-semibold text-4xl">Greyana Sofa</h1>
+                <h1 class="font-semibold text-4xl text-purple-700">Greyana Sofa</h1>
                 <p class="mt-4">Introducing the Greyana Sofa. The perfect combination of style and comfort. With its sleek and modern design, this sofa is a statement piece that will elevate any living space. The Greyana Sofa features a stunning grey upholstery that is both soft to the touch and durable for everyday use. Crafted with a sturdy wooden frame and high-density foam cushions, the Greyana Sofa offers unbeatable comfort and support. The seat and back cushions are also removable and reversible, making it easy to maintain and keep looking like new.</p>
             </div>
             <!-- layout 2b -->
